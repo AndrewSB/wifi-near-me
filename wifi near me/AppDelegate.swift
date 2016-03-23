89fd8eedeaa9db910c4d5b1c68075e7e2ec4b79b
Andrew Breckenridge <asbreckenridge@me.com>
2016-03-22 17:26:36 -0700

    add Fabric and Swiftgen

--- wifi near me/AppDelegate.swift
+++ wifi near me/AppDelegate.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+import Fabric
+import Crashlytics
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,7 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
+        Fabric.with([Crashlytics.self])
         
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window!.rootViewController = ViewController()
         
         return true
     }
