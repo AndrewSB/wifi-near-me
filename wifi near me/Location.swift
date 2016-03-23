113b48e6deabe4a640bbaf5bc4a91e6c12748ac6
Andrew Breckenridge <asbreckenridge@me.com>
2016-03-22 17:07:26 -0700

    init

---
+++ wifi near me/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  wifi near me
+//
+//  Created by Andrew Breckenridge on 3/22/16.
+//  Copyright © 2016 Andrew Breckenridge. All rights reserved.
+//
+
+import Foundation
+
+class Location {
+    
+}
